6af97ef523deab297d239dfd5d4b6daf6d6c80fd
Sara Ghaemi <ghaemi.sr@gmail.com>
2016-06-29 15:56:26 +0430

    FinalGUI and plot + first classification test - Without FinalExam

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy as np
-
-
-extensions = [Extension("calCython", ["calCython.pyx"],
-                        language="c++",
-						include_dirs=[np.get_include()])]
-setup(ext_modules=cythonize(extensions, annotate=True))
